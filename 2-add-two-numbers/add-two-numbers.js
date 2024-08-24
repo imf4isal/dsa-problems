3f09375772f5c8d740c45c3a276ab77ad0a2027b
Faisal <faisal_abdullah99@yahoo.com>
2024-08-24 12:22:20 +0600

    Time: 112 ms (40.56%) | Memory: 56 MB (40.79%) - LeetSync

---
+++ 2-add-two-numbers/add-two-numbers.js
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+    
+    const dummy = new ListNode();
+    let current = dummy;
+    let carry = 0;
+
+    while(l1 || l2 || carry){
+        let sum = 0;
+
+        if(l1){
+            sum += l1.val;
+            l1 = l1.next;
+        }
+
+        if(l2) {
+            sum += l2.val;
+            l2 = l2.next;
+        }
+
+        sum += carry;
+
+        carry = Math.floor(sum/10);
+        const node = new ListNode(sum%10)
+
+        current.next = node;
+        current = node;
+    }
+
+    return dummy.next;
+
+};
