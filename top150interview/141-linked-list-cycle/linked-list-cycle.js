e2e3c026acbec19f84f3a56fad2284bf9db3d791
Faisal <mdfaisal.dev@gmail.com>
2025-06-21 08:47:38 +0600

    Time: 44 ms (91.33%) | Memory: 58.9 MB (71.28%) - LeetSync

---
+++ top150interview/141-linked-list-cycle/linked-list-cycle.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    if(!head) return false;
+
+    let tortoise = head;
+    let hare = head.next;
+
+    while (hare && hare.next) {
+        if(tortoise === hare) return true;
+
+        tortoise = tortoise.next;
+        hare = hare.next.next;
+    }
+    return false;
+};
