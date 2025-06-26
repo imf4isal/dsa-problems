902945d258867599b03b708eeb066dc208c82b1e
Faisal <mdfaisal.dev@gmail.com>
2025-06-26 07:36:11 +0600

    Time: 1 ms (26.71%) | Memory: 56.3 MB (76.37%) - LeetSync

---
+++ top150interview/61-rotate-list/rotate-list.js
@@ -0,0 +1,29 @@
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+var rotateRight = function(head, k) {
+    if (!head || k === 0) return head;
+
+    let length = 1;
+    let tail = head;
+    while (tail.next) {
+        tail = tail.next;
+        length++;
+    }
+
+    k = k % length;
+    if (k === 0) return head;
+
+    tail.next = head; // form a cycle
+    let stepsToNewHead = length - k;
+    let newTail = tail;
+    while (stepsToNewHead--) {
+        newTail = newTail.next;
+    }
+
+    let newHead = newTail.next;
+    newTail.next = null;
+    return newHead;
+};
