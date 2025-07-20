724753dc099385c696d4f7dc20e549b150949509
Faisal <mdfaisal.dev@gmail.com>
2025-07-20 07:34:46 +0600

    Time: 1 ms (39.91%) | Memory: 57.9 MB (41.04%) - LeetSync

--- top150interview/21-merge-two-sorted-lists/merge-two-sorted-lists.js
+++ top150interview/21-merge-two-sorted-lists/merge-two-sorted-lists.js
@@ -17,8 +17,7 @@ var mergeTwoLists = function(list1, list2) {
     if(list1.val <= list2.val){
         list1.next = mergeTwoLists(list1.next, list2);
         return list1;
-    }
-    if(list2.val < list1.val){
+    }else {
         list2.next = mergeTwoLists(list1, list2.next);
         return list2;
     }
