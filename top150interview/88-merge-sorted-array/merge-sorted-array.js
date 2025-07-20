22942b3d64069486d6292a0c0878627e3fb762a6
Faisal <mdfaisal.dev@gmail.com>
2025-07-20 08:35:04 +0600

    Time: 0 ms (100.00%) | Memory: 56.4 MB (5.10%) - LeetSync

--- top150interview/88-merge-sorted-array/merge-sorted-array.js
+++ top150interview/88-merge-sorted-array/merge-sorted-array.js
@@ -6,8 +6,19 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-    for(let i = m, j=0; i < m+n; i++, j++){
-        nums1[i] = nums2[j];
+    let i = m - 1;
+    let j = n - 1;
+    let k = m + n - 1;
+
+    while(i>=0 && j>=0){
+        if(nums1[i] > nums2[j]){
+            nums1[k--] = nums1[i--]
+        }else{
+            nums1[k--] = nums2[j--]
+        }
     }
-    nums1.sort((a,b) => a-b)
+
+    while(j>=0){
+        nums1[k--] = nums2[j--]
+    }
 };
