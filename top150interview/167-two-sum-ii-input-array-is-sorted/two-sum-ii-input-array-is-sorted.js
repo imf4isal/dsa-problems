1439bd931cb9f4a8acdeb54b47b76c86ffa025ae
Faisal <mdfaisal.dev@gmail.com>
2026-01-01 22:02:02 +0600

    Time: 0 ms (100.00%) | Memory: 56.3 MB (22.07%) - LeetSync

--- top150interview/167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
+++ top150interview/167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
@@ -4,16 +4,13 @@
  * @return {number[]}
  */
 var twoSum = function(numbers, target) {
-    let left = 0;
-    let right = numbers.length - 1;
+    let left = 0, right = numbers.length - 1;
 
-    while(left < right){
-        let sum = numbers[left]+numbers[right];
-        if(sum < target) left++;
-        else if(sum > target) right--;
-        else {
-            return [left+1, right+1]
-        }
+    while(left<right){
+        let sum = numbers[left] + numbers[right];
+        if(sum<target) left++;
+        else if (sum>target) right--;
+        else return [left+1, right+1];
     }
     return [];
 };
