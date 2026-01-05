8cc2c8cf0ed78895aed4b9288e97f9900c9cfa6b
Faisal <mdfaisal.dev@gmail.com>
2026-01-05 17:42:15 +0600

    Time: 46 ms (23.12%) | Memory: 75.2 MB (5.11%) - LeetSync

--- top150interview/15-3sum/3sum.js
+++ top150interview/15-3sum/3sum.js
@@ -1,32 +1,43 @@
-function threeSum(nums) {
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+
+var twoSum = function(nums, start, target) {
+  const pairs = [];
+  let left = start, right = nums.length - 1;
+
+  while (left < right) {
+    const sum = nums[left] + nums[right];
+
+    if (sum === target) {
+      pairs.push([nums[left], nums[right]]);
+      left++;
+      right--;
+
+      while (left < right && nums[left] === nums[left - 1]) left++;
+    //   while (left < right && nums[right] === nums[right + 1]) right--;
+    } else if (sum < target) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+  return pairs;
+};
+
+var threeSum = function(nums) {
   const triplets = [];
   nums.sort((a, b) => a - b);
 
-  for (let i = 0; i < nums.length - 2; i++) {
+  for (let i = 0; i < nums.length; i++) {
     if (nums[i] > 0) break;
     if (i > 0 && nums[i] === nums[i - 1]) continue;
 
-    const target = -nums[i];
-    let left = i + 1;
-    let right = nums.length - 1;
-
-    while (left < right) {
-      const sum = nums[left] + nums[right];
-      if (sum === target) {
-        triplets.push([nums[i], nums[left], nums[right]]);
-        const leftVal = nums[left];
-        const rightVal = nums[right];
-
-        while (left < right && nums[left] === leftVal) left++;
-        while (left < right && nums[right] === rightVal) right--;
-        
-      } else if (sum < target) {
-        left++;
-      } else {
-        right--;
-      }
+    const pairs = twoSum(nums, i + 1, -nums[i]);
+    for (const pair of pairs) {
+      triplets.push([nums[i], ...pair]);
     }
   }
-
   return triplets;
-}
+};
