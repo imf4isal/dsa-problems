4b35d5f4358c5df113db5fd86379c37cbb9ff821
Faisal <mdfaisal.dev@gmail.com>
2026-01-06 15:25:06 +0600

    Time: 3 ms (59.12%) | Memory: 55.8 MB (53.21%) - LeetSync

--- top150interview/1-two-sum/two-sum.js
+++ top150interview/1-two-sum/two-sum.js
@@ -7,11 +7,8 @@ var twoSum = function(nums, target) {
     const hash = new Map();
 
     for(let i = 0; i < nums.length; i++){
-        if(hash.has(target-nums[i])){
-            return [hash.get(target-nums[i]), i];
-        }
-        hash.set(nums[i], i);
+        if(hash.has(target-nums[i])) return [hash.get(target-nums[i]), i];
+        else hash.set(nums[i], i);
     }
     return [];
-    
 };
