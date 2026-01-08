84250e93a49e87c5ed4e7b453290f7f7b815e2bf
Faisal <mdfaisal.dev@gmail.com>
2026-01-08 10:57:53 +0600

    Time: 55 ms (61.79%) | Memory: 58.1 MB (90.70%) - LeetSync

--- top150interview/141-linked-list-cycle/linked-list-cycle.js
+++ top150interview/141-linked-list-cycle/linked-list-cycle.js
@@ -11,17 +11,12 @@
  * @return {boolean}
  */
 var hasCycle = function(head) {
-    if(!head || !head.next) return false;
+    let slow = head, fast = head;
 
-    let slow = head;
-    let fast = head.next;
-    
-    while(fast && fast.next){
+    while(fast && fast.next) {
         slow = slow.next;
         fast = fast.next.next;
-
         if(slow === fast) return true;
     }
     return false;
-
 };
